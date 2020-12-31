fff4f73453c05f58d74c21a921b277b0a3734731
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-12-30 20:43:52 -0600

    added computerAI factory function

--- src/__tests__/computerAI.test.js
+++ src/__tests__/computerAI.test.js
@@ -6,3 +6,27 @@ test('AI makes a random move', () => {
   let pos = COM1.fire(COM2.gameboard);
   expect(COM2.gameboard.misses).toEqual([pos]);
 });
+
+test('AI makes a random legal move(1)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  for (let i = 1; i < 100; i++) {
+    COM2.gameboard.board[i] = 'hit'
+  }
+  COM1.fire(COM2.gameboard);
+  expect(COM2.gameboard.board[0]).toBe('miss');
+});
+
+test('AI makes a random legal move(2)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  for (let i = 0; i < 100; i++) {
+    COM2.gameboard.board[i] = 'miss'
+  };
+  COM2.gameboard.placeShip(1, 'x', COM2.gameboard.ships[0]);
+  COM1.fire(COM2.gameboard);
+  COM1.fire(COM2.gameboard);
+  let array = [COM2.gameboard.board[1]]
+  array.push(COM2.gameboard.board[2])
+  expect(array).toEqual(['hit', 'hit']);
+});
--- src/factory functions/computerAI.js
+++ src/factory functions/computerAI.js
@@ -2,9 +2,14 @@ import Gameboard from './gameboard';
 
 function AI() {
   function fire(enemyBoard) {
-    
+    let pos = Math.floor(Math.random() * 100);
+    while (enemyBoard.board[pos] !== undefined && enemyBoard.board[pos] !== 'ship') {
+      pos = Math.floor(Math.random() * 100);
+    }
+    enemyBoard.receiveAttack(pos);
+    return pos;
   };
-  
+
   return {
     name: 'COM',
     gameboard: Gameboard(),
