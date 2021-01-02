887606c71045c452b3da2faf21bfdef59a970174
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-01-02 16:12:22 -0600

    added enemy board

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 import './App.css';
 import Player from "./factory functions/player";
+import AI from "./factory functions/computerAI";
 import Board from "./components/Board";
 const player1 = Player('player1');
+const COM = AI();
 
 function App() {
-  const [board, setBoard] = useState(player1.gameboard.board);
+  const [board] = useState(player1.gameboard.board);
+  const [enemyBoard, setEnemyBoard] = useState(COM.gameboard.board);
 
   function updateCell(id) {
-    player1.gameboard.receiveAttack(id);
-    setBoard([...player1.gameboard.board]);
+    player1.fire(id, COM.gameboard);
+    setEnemyBoard([...COM.gameboard.board]);
   };
 
   return (
     <div className="App">
-      <Board board={board} updateCell={updateCell}/>
+      <Board board={board} updateCell={updateCell} isEnemy={false}/>
+      <Board board={enemyBoard} updateCell={updateCell} isEnemy={true}/>
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
 function Board(props) {
+  function boardType() {
+    if (props.isEnemy === true) {
+      return (
+        props.board.map((cell, id) => (
+          <span key={id} onClick={() => props.updateCell(id)}>{cell === 'miss' ? '⬤' : ''}</span>
+        ))
+      );
+    } else {
+      return (
+        props.board.map((cell, id) => (
+          <span key={id}>{cell === 'miss' ? '⬤' : ''}</span>
+        ))
+      );
+    }
+  }
 
   return (
     <div className='board'>
-      {props.board.map((cell, id) => (
-        <span key={id} onClick={() => props.updateCell(id)}>{cell === 'miss' ? '⬤' : ''}</span>
-      ))}
+      {boardType()}
     </div>
   );
 };
