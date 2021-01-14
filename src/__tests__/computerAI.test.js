e59e8e8931d3560f844693f05fb72caa68f85bac
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-01-13 19:06:40 -0600

    improvements to AI

--- src/__tests__/computerAI.test.js
+++ src/__tests__/computerAI.test.js
@@ -40,3 +40,131 @@ test('AI places ships in a random position', () => {
   expect(COM1.gameboard.ships[3].boardPosition).not.toEqual([]);
   expect(COM1.gameboard.ships[4].boardPosition).not.toEqual([]);
 });
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(1)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(0, 'y', COM2.gameboard.ships[0]);
+  COM2.gameboard.receiveAttack(10);
+  COM1.setCurrentTarget(10);
+  expect(COM1.fire(COM2.gameboard)).toBe(11);
+  expect(COM1.fire(COM2.gameboard)).toBe(20);
+  expect(COM1.fire(COM2.gameboard)).toBe(0);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(2)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(17, 'x', COM2.gameboard.ships[1]);
+  COM2.gameboard.receiveAttack(17);
+  COM1.setCurrentTarget(17);
+  expect(COM1.fire(COM2.gameboard)).toBe(18);
+  expect(COM1.fire(COM2.gameboard)).toBe(19);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(3)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(50, 'y', COM2.gameboard.ships[4]);
+  COM2.gameboard.receiveAttack(90);
+  COM1.setCurrentTarget(90);
+  expect(COM1.fire(COM2.gameboard)).toBe(91);
+  expect(COM1.fire(COM2.gameboard)).toBe(80);
+  expect(COM1.fire(COM2.gameboard)).toBe(70);
+  expect(COM1.fire(COM2.gameboard)).toBe(60);
+  expect(COM1.fire(COM2.gameboard)).toBe(50);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(4)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(25, 'y', COM2.gameboard.ships[3]);
+  COM2.gameboard.receiveAttack(35);
+  COM1.setCurrentTarget(35);
+  expect(COM1.fire(COM2.gameboard)).toBe(36);
+  expect(COM1.fire(COM2.gameboard)).toBe(34);
+  expect(COM1.fire(COM2.gameboard)).toBe(45);
+  expect(COM1.fire(COM2.gameboard)).toBe(55);
+  expect(COM1.fire(COM2.gameboard)).toBe(65);
+  expect(COM1.fire(COM2.gameboard)).toBe(25);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(5)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(65, 'y', COM2.gameboard.ships[0]);
+  COM2.gameboard.placeShip(95, 'x', COM2.gameboard.ships[4]);
+  COM2.gameboard.receiveAttack(75);
+  COM1.setCurrentTarget(75);
+  expect(COM1.fire(COM2.gameboard)).toBe(76);
+  expect(COM1.fire(COM2.gameboard)).toBe(74);
+  expect(COM1.fire(COM2.gameboard)).toBe(85);
+  expect(COM1.fire(COM2.gameboard)).toBe(65);
+  COM1.fire(COM2.gameboard);
+  COM2.gameboard.receiveAttack(98);
+  COM1.setCurrentTarget(98);
+  expect(COM1.fire(COM2.gameboard)).toBe(99);
+  expect(COM1.fire(COM2.gameboard)).toBe(97);
+  expect(COM1.fire(COM2.gameboard)).toBe(96);
+  expect(COM1.fire(COM2.gameboard)).toBe(95);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(6)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(49, 'y', COM2.gameboard.ships[2]);
+  COM2.gameboard.receiveAttack(69);
+  COM1.setCurrentTarget(69);
+  expect(COM1.fire(COM2.gameboard)).toBe(68);
+  expect(COM1.fire(COM2.gameboard)).toBe(79);
+  expect(COM1.fire(COM2.gameboard)).toBe(59);
+  expect(COM1.fire(COM2.gameboard)).toBe(49);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(7)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(40, 'y', COM2.gameboard.ships[4]);
+  COM2.gameboard.receiveAttack(80);
+  COM1.setCurrentTarget(80);
+  expect(COM1.fire(COM2.gameboard)).toBe(81);
+  expect(COM1.fire(COM2.gameboard)).toBe(90);
+  expect(COM1.fire(COM2.gameboard)).toBe(70);
+  expect(COM1.fire(COM2.gameboard)).toBe(60);
+  expect(COM1.fire(COM2.gameboard)).toBe(50);
+  expect(COM1.fire(COM2.gameboard)).toBe(40);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(8)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(1, 'y', COM2.gameboard.ships[3]);
+  COM2.gameboard.placeShip(40, 'x', COM2.gameboard.ships[1]);
+  COM2.gameboard.receiveAttack(40);
+  COM2.gameboard.receiveAttack(41);
+  COM2.gameboard.receiveAttack(42);
+  COM2.gameboard.receiveAttack(21);
+  COM1.setCurrentTarget(21);
+  expect(COM1.fire(COM2.gameboard)).toBe(22);
+  expect(COM1.fire(COM2.gameboard)).toBe(20);
+  expect(COM1.fire(COM2.gameboard)).toBe(31);
+  expect(COM1.fire(COM2.gameboard)).toBe(11);
+  expect(COM1.fire(COM2.gameboard)).toBe(1);
+});
+
+test('when the AI hits a ship, the next attack will be adjacent to the last hit(9)', () => {
+  let COM1 = AI();
+  let COM2 = AI();
+  COM2.gameboard.placeShip(1, 'y', COM2.gameboard.ships[3]);
+  COM2.gameboard.placeShip(40, 'x', COM2.gameboard.ships[1]);
+  COM2.gameboard.receiveAttack(40);
+  COM2.gameboard.receiveAttack(41);
+  COM2.gameboard.receiveAttack(42);
+  COM2.gameboard.receiveAttack(31);
+  COM1.setCurrentTarget(31);
+  expect(COM1.fire(COM2.gameboard)).toBe(32);
+  expect(COM1.fire(COM2.gameboard)).toBe(30);
+  expect(COM1.fire(COM2.gameboard)).toBe(21);
+  expect(COM1.fire(COM2.gameboard)).toBe(11);
+  expect(COM1.fire(COM2.gameboard)).toBe(1);
+});
--- src/factory functions/computerAI.js
+++ src/factory functions/computerAI.js
@@ -2,13 +2,67 @@ import Gameboard from './gameboard';
 import autoPlaceShips from './autoPlaceShips';
 
 function AI() {
+  let currentTarget;
+  let nextPos;
+  let previousTarget;
+
   function fire(enemyBoard) {
-    let pos = Math.floor(Math.random() * 100);
-    while (enemyBoard.board[pos] !== '' && enemyBoard.board[pos] !== 'ship') {
-      pos = Math.floor(Math.random() * 100);
+    let pos = currentTarget ? pickNextTarget(enemyBoard) : Math.floor(Math.random() * 100);
+    if(!(currentTarget)) {
+      while (enemyBoard.board[pos] !== '' && enemyBoard.board[pos] !== 'ship') {
+        pos = Math.floor(Math.random() * 100);
+      }
     }
     enemyBoard.receiveAttack(pos);
+    if (enemyBoard.board[pos] === 'hit') {
+      currentTarget = pos;
+      if (!(previousTarget)) {
+        previousTarget = pos;
+      }
+    }
     return pos;
+  };
+
+  function setCurrentTarget(pos) {
+    currentTarget = pos;
+    previousTarget = pos;
+  };
+
+  function pickNextTarget(enemyBoard) {
+    function validPos(pos) {
+      if (enemyBoard.board[currentTarget + pos] !== 'miss' && enemyBoard.board[currentTarget + pos] !== 'hit' && currentTarget + pos < 100) {
+        if (pos === 1 && currentTarget % 10 + 1 > 9) {
+          return false;
+        } else if (pos === -1 && currentTarget % 10 - 1 < 0) {
+          return false;
+        } else {
+          return true;
+        }
+      }
+    };
+
+    if (isEnemyShipSunk(currentTarget, enemyBoard.ships)) {
+      currentTarget = undefined;
+      previousTarget = undefined;
+      nextPos = undefined;
+    } else {
+      const possiblePos = [1, -1, 10, -10];
+      if (!(enemyBoard.board[currentTarget + nextPos] !== 'miss' && enemyBoard.board[currentTarget + nextPos] !== 'hit' && currentTarget + nextPos < 100)) {
+        if (enemyBoard.board[previousTarget - nextPos] === 'ship' && nextPos) {
+          currentTarget = previousTarget;
+          nextPos = -nextPos;
+        } else {
+          nextPos = possiblePos.find((pos) => (validPos(pos)));
+        }
+      }
+      return currentTarget + nextPos
+    }
+  };
+
+  function isEnemyShipSunk(pos, ships) {
+    return ships.filter(ship => ship.isSunk()).some((ship) => (
+      ship.boardPosition.includes(pos)
+    ));
   };
 
   return {
@@ -16,6 +70,7 @@ function AI() {
     gameboard: Gameboard(),
     autoPlaceShips,
     fire,
+    setCurrentTarget,
   };
 };
 
